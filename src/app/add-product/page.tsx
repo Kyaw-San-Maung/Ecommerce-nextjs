de7cd03be6d710c0f0c3536c9329aae0212f5155
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-02-12 13:22:23 +0630

    Creating form for adding data.

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,7 +1,39 @@
 export default function AddProduct() {
   return (
     <div>
-      <h1 className="text-lg">Add Product</h1>
+      <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+      <form action="">
+        <input
+          type="text"
+          required
+          name="name"
+          placeholder="Name"
+          className="input-bordered mb-3 w-full input  "
+        />
+        <textarea
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+          required
+        />
+        <input
+          type="url"
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          className="input-bordered mb-3 w-full input  "
+        />
+        <input
+          type="number"
+          required
+          name="price"
+          placeholder="Price"
+          className="input-bordered mb-3 w-full input  "
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Add Product
+        </button>
+      </form>
     </div>
   );
 }
