a31ba3a8fd5c8518bf355f42cc6438f64baab01d
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-02-13 21:44:50 +0630

    Create Loading page for loading process.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,7 @@
 export default function LoadingPage() {
-  return <span className="loading loading-dots loading-lg m-auto block " />;
+  return (
+    <div className="w-full">
+      <span className="loading loading-dots loading-lg m-auto block " />
+    </div>
+  );
 }
